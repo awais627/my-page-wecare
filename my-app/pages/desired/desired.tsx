5f2a29989c7c056b4dc0603f3c76ce8ca73e80cb
talk2awais <muhammad.awais@southvillesol.com>
2022-06-27 20:18:28 +0500

    Create Desired information compoenent

---
+++ my-app/pages/desired/desired.tsx
@@ -0,0 +1,47 @@
+import styles from "../../styles/desired/desired.module.scss";
+import edit from "../../public/desired/edit.svg";
+import Image from "next/image";
+const Desired = () => {
+  return (
+    <>
+      <div className={styles.main}>
+        <div>
+          <span>Desired Condition</span>
+          <div>
+            <Image src={edit} alt="Edit" />
+            <span>Edit</span>
+          </div>
+        </div>
+        <div className={styles.wrapper}>
+          <div>Occupation</div>
+          <div className={styles.occupation}>
+            <div className={styles.not}>Clinical laboratory technician</div>
+            <div>Dental hygienist / dental technician</div>
+            <div>Dispensing pharmacy office</div>
+            <div>Childminder</div>
+            <div>Operation manager </div>
+            <div>others</div>
+          </div>
+        </div>
+        <div className={styles.wrapper}>
+          <div>Work Location</div>
+          <div>Chuo-ku, Hokkaido</div>
+        </div>
+        <div className={styles.wrapper}>
+          <div>Work Style</div>
+          <div>Full-time employee</div>
+        </div>
+        <div className={styles.wrapper}>
+          <div>Facility form</div>
+          <div>General hospital</div>
+        </div>
+        <div className={styles.wrapper}>
+          <div>Annual income</div>
+          <div>Eos sed ut laboris 10,000 yen</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Desired;
