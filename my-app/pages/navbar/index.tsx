550ee92aa7e0d329999b31ce8c9201bc7beb76aa
talk2awais <muhammad.awais@southvillesol.com>
2022-06-28 13:19:24 +0500

    Create navbar component

---
+++ my-app/pages/navbar/index.tsx
@@ -0,0 +1,25 @@
+import logo from "../../public/navbar/wecare_logo.svg";
+import styles from "../../styles/navbar/navbar.module.scss";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className={styles.main}>
+        <div className={styles.logo}>
+          <Image src={logo} alt="Wecare" width={150} height={100} />
+        </div>
+        <div className={styles.navlinks}>
+          <ul>
+            <li>Job search</li>
+            <li>be on one's mind!</li>
+            <li>message</li>
+            <li>My page</li>
+            <li>N</li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+export default Navbar;
