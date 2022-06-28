2b217d4d8b75982321dedca6f7ae0218c2bbce3c
talk2awais <muhammad.awais@southvillesol.com>
2022-06-28 17:13:49 +0500

    Create container

--- my-app/pages/mypage/userinput.tsx
+++ my-app/pages/mypage/userinput.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import styles from "../../styles/mypage/userinput.module.scss";
 import { FC } from "react";
+import edit from "../../public/mypage/edit.svg";
 interface UserProps {
   heading: string;
   img: string;
   imgText: string;
+  qualification?: any;
 }
-const UserData: FC<UserProps> = ({ heading, img, imgText }) => {
+const UserData: FC<UserProps> = ({ heading, img, imgText, qualification }) => {
   return (
     <>
       <div className={styles.main}>
@@ -16,6 +18,28 @@ const UserData: FC<UserProps> = ({ heading, img, imgText }) => {
             <Image src={img} alt="edit" width={20} height={20} />
             <span>{imgText}</span>
           </div>
+        </div>
+        <div>
+          {qualification ? (
+            <>
+              <div className={styles.headerData}>
+                <div>Speech therapist(ST) 54321</div>
+                <div>
+                  <Image src={edit} alt="edit" width={20} height={20} />
+                  <span>Edit</span>
+                </div>
+              </div>
+              <div className={styles.headerData}>
+                <div>Speech therapist(ST) 123456</div>
+                <div>
+                  <Image src={edit} alt="edit" width={20} height={20} />
+                  <span>Edit</span>
+                </div>
+              </div>
+            </>
+          ) : (
+            <></>
+          )}
         </div>
       </div>
     </>
