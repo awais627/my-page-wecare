b60c997542bb2c20f362ce88a057fd85947790f6
talk2awais <muhammad.awais@southvillesol.com>
2022-06-29 12:40:50 +0500

    Apply navbar chnages  requested

--- my-app/pages/navbar/index.tsx
+++ my-app/pages/navbar/index.tsx
@@ -1,18 +1,19 @@
 import logo from "../../public/assets/images/wecare_logo.svg";
 import styles from "./navbar.module.scss";
 import Image from "next/image";
+import { FC } from "react";
 
-const Navbar = () => {
+const Navbar: FC = () => {
   return (
     <>
       <nav className={styles.main}>
-        <div className={styles.logo}>
+        <div>
           <Image src={logo} alt="Wecare" width={150} height={100} />
         </div>
         <div className={styles.navLinks}>
           <ul>
             <li>Job search</li>
-            <li>be on one's mind!</li>
+            <li>be on one&apos;s mind!</li>
             <li>message</li>
             <li>My page</li>
             <li>N</li>
